9c73d37e3bb3447226238dd754eef657ccfea8a7
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-07-06 14:40:50 +0700

    update Todo.tsx

--- src/Components/Todo.tsx
+++ src/Components/Todo.tsx
@@ -1,3 +1,15 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPenToSquare } from "@fortawesome/free-regular-svg-icons";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+
 export const Todo = () => {
-   return <div>Todo</div>;
+   return (
+      <div className='Todo'>
+         <p>Go to school</p>
+         <div>
+            <FontAwesomeIcon icon={faPenToSquare} />
+            <FontAwesomeIcon icon={faTrash} />
+         </div>
+      </div>
+   );
 };
