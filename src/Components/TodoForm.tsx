1c98a4f020bf992e03eff821852f83712e9e431b
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-07-06 16:28:36 +0700

    fix error type data

--- src/Components/TodoForm.tsx
+++ src/Components/TodoForm.tsx
@@ -2,13 +2,12 @@ import { useState } from "react";
 
 //sekarang masalahnya adalah kita memiliki nilai ini di sini, dan kita harus menerukan nilai ini ke status to do, dan kita bisa simpan status disini karena komponen lain perlu mengakses status juga jadi yang perlu kita lakukan sebenarnya adalah membuat status di komponen induk yang merupakan pembungkus tugas
 
-export const TodoForm = () => {
+export const TodoForm = ({ addTodo }: { addTodo: any }) => {
    const [value, setValue] = useState("");
    const handleSubmit = (e: { preventDefault: () => void }) => {
       e.preventDefault();
 
-      console.log(value);
-      setValue("");
+      addTodo(value);
    };
    return (
       <form onSubmit={handleSubmit}>
--- src/Components/TodoWrapper.tsx
+++ src/Components/TodoWrapper.tsx
@@ -1,9 +1,31 @@
+import { useState } from "react";
 import { TodoForm } from "./TodoForm";
+import { v4 as uuidv4 } from "uuid";
+uuidv4();
+
+interface Todo {
+   id: string;
+   task: string;
+   completed: boolean;
+   isEditing: boolean;
+}
 
 export const TodoWrapper = () => {
+   const [todos, setTodos] = useState<Todo[]>([]);
+
+   const addTodo = (todo: any) => {
+      const newTodo: Todo = {
+         id: uuidv4(),
+         task: todo,
+         completed: false,
+         isEditing: false,
+      };
+      setTodos((prevTodos) => [...prevTodos, newTodo]);
+      console.log(todos);
+   };
    return (
       <div className='TodoWrapper'>
-         <TodoForm />
+         <TodoForm addTodo={addTodo} />
       </div>
    );
 };
