3292e42656a20972291dfbdf14efa9a0650bba7e
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-07-06 15:50:17 +0700

    remake TodoWrapper

--- src/Components/TodoWrapper.tsx
+++ src/Components/TodoWrapper.tsx
@@ -1,11 +1,9 @@
 import { TodoForm } from "./TodoForm";
-import { Todo } from "./Todo";
 
 export const TodoWrapper = () => {
    return (
       <div className='TodoWrapper'>
          <TodoForm />
-         <Todo />
       </div>
    );
 };
