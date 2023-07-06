bee88fb5ffa82c4b93be50e52cb50bc3cdd24ef3
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-07-06 08:32:21 +0700

    try useState

--- src/Components/TodoForm.tsx
+++ src/Components/TodoForm.tsx
@@ -1,10 +1,14 @@
+import { useState } from "react";
+
 export const TodoForm = () => {
+   const [value, setValue] = useState("");
    return (
       <form>
          <input
             type='text'
             className='todo-input'
             placeholder='What is the task today'
+            onChange={(e) => console.log(e.target.value)}
          />
          <button type='submit' className='todo-btn'>
             Task
