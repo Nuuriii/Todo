2ecf5f35f91718cd4197f21bf411f6380283c8bd
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-07-06 15:21:17 +0700

    fix error

--- src/Components/TodoForm.tsx
+++ src/Components/TodoForm.tsx
@@ -4,19 +4,19 @@ import { useState } from "react";
 
 export const TodoForm = () => {
    const [value, setValue] = useState("");
-
    const handleSubmit = (e: { preventDefault: () => void }) => {
       e.preventDefault();
 
       console.log(value);
+      setValue("");
    };
    return (
       <form onSubmit={handleSubmit}>
          <input
             type='text'
             className='todo-input'
-            placeholder='What is the task today'
             onChange={(e) => setValue(e.target.value)}
+            placeholder='What is the task today'
          />
          <button type='submit' className='todo-btn'>
             Task
--- src/Components/TodoWrapper.tsx
+++ src/Components/TodoWrapper.tsx
@@ -1,9 +1,11 @@
 import { TodoForm } from "./TodoForm";
+import { Todo } from "./Todo";
 
 export const TodoWrapper = () => {
    return (
       <div className='TodoWrapper'>
          <TodoForm />
+         <Todo />
       </div>
    );
 };
