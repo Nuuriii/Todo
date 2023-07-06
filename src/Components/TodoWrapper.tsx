1a62c28de1d6cafb303191d20f6fbd6e7d0fe108
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-07-06 17:01:09 +0700

    feat: add comment for Component

--- src/Components/TodoWrapper.tsx
+++ src/Components/TodoWrapper.tsx
@@ -3,6 +3,8 @@ import { TodoForm } from "./TodoForm";
 import { v4 as uuidv4 } from "uuid";
 uuidv4();
 
+//Sebelum ini ada error yang berhubungan dengan tipe data
+
 interface Todo {
    id: string;
    task: string;
@@ -11,8 +13,18 @@ interface Todo {
 }
 
 export const TodoWrapper = () => {
+   //Sebelum
+   /* const [todos, setTodos] = useState([]); */
+   // Sesudah
    const [todos, setTodos] = useState<Todo[]>([]);
 
+   //Sebelum
+   /* const addTodo = (todo: any) => {
+   setTodos([
+      ...todos,
+      { id: uuidv4(), task: todo, completed: false, isEditing: false },
+   ]); */
+   //Sesudah
    const addTodo = (todo: any) => {
       const newTodo: Todo = {
          id: uuidv4(),
