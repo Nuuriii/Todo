da5c7830552d207ab53e2a1b5543635504b8c05e
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-07-07 19:47:05 +0700

    feat: add comment for description div

--- src/Components/Todo.tsx
+++ src/Components/Todo.tsx
@@ -6,6 +6,7 @@ export const Todo = () => {
    return (
       <div className='Todo'>
          <p>Go to school</p>
+         {/* Dibawah ini untuk menyimpan Icon */}
          <div>
             <FontAwesomeIcon icon={faPenToSquare} />
             <FontAwesomeIcon icon={faTrash} />
