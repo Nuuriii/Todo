1dfebb1b4bfa45ca7ef5d57984598501964245a8
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-07-07 20:56:54 +0700

    feat: add onClick property & add className

--- src/Components/Todo.tsx
+++ src/Components/Todo.tsx
@@ -2,10 +2,18 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPenToSquare } from "@fortawesome/free-regular-svg-icons";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
-export const Todo = ({ task }: { task: any }) => {
+export const Todo = (
+   { task }: { task: any },
+   { toggleComplete }: { toggleComplete: any }
+) => {
    return (
       <div className='Todo'>
-         <p>{task.task}</p>
+         <p
+            onClick={() => toggleComplete(task.id)}
+            className={`${task.completed ? "completed" : ""}`}
+         >
+            {task.task}
+         </p>
          {/* Dibawah ini untuk menyimpan Icon */}
          <div>
             <FontAwesomeIcon icon={faPenToSquare} />
